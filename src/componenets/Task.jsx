78c33e6511509f3fdafbd992f7757fa3af8504a9
fareska <fares.xb@gmail.com>
2021-08-10 10:11:13 +0300

    more styles

--- src/componenets/Task.jsx
+++ src/componenets/Task.jsx
@@ -8,9 +8,9 @@ export default function Task(props) {
     }
 
     return (
-        <div>
+        <div id="taskContainer" className={`bg-white m-2 flex justify-between  ${props.task.isCompleted ? 'bg-green-400' : 'bg-white'}`} >
             {props.task.task}
-            <input onClick={completeHandler}  type="checkbox" name="" id={`${props.id}`} />
+            <div onClick={completeHandler} id={`${props.id}`} className="w-8 h-5.5 bg-green-400 "> V </div>
         </div>
     )
 }
